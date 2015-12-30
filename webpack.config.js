b832aa74f6064e2f30d10a6e7bce24870b232167
Your Name <you@example.com>
2015-12-30 16:14:09 +0000

    setup typescript

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ module.exports = {
     output: {
         filename: "bundle.js"
     },
+    devtool: 'source-map',
     resolve: {
         extensions: ['', '.webpack.js', '.web.js', '.ts',  '.js']
     },
