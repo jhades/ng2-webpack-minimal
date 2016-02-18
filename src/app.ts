bc687a160042342e21050ab1ea49b9c1ce78113f
JHades <jhades.dev@gmail.com>
2016-02-18 12:25:22 +0100

    update to beta-6

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
+
 import 'angular2/bundles/angular2-polyfills';
 import {Component} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
